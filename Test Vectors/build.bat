3f8c732e9b9158746406205f52111901649d48b8
John Mattsson <john.mattsson@ericsson.com>
2020-02-18 12:06:46 +0100

    Removed -fpermissive
    Removed -fpermissive 
    The CCM code has been modified with explicit casts

--- Test Vectors/build.bat
+++ Test Vectors/build.bat
@@ -1,1 +1,1 @@
-g++ -fpermissive aes.c aes-ccm.c aes-enc.c vectors.cpp -lsodium -o vectors.exe -Ilibsodium-win64\include -Llibsodium-win64\lib
+g++ aes.c aes-ccm.c aes-enc.c vectors.cpp -lsodium -o vectors.exe -Ilibsodium-win64\include -Llibsodium-win64\lib
